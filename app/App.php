d6fa8793d786867e01f9942b78a53f62833a4f89
Tiburce Kouagou <kouagoutiburce@gmail.com>
2023-06-23 18:58:29 +0200

    implemented the functionality to get file data

--- app/App.php
+++ app/App.php
@@ -3,3 +3,34 @@
 declare(strict_types = 1);
 
 // Your code
+function getTransactionFiles(string $dirPath): array {
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        } 
+
+        $files[] = FILES_PATH . $file;
+
+        return $files;
+    }
+}
+
+function getTransactions(string $fileName): array {
+    if (!file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exists.', E_USER_ERROR);
+    }
+    
+    $file = fopen($fileName, 'r');
+
+    $transactions = [];
+
+    fgetcsv($file);
+    
+    while(($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,14 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 // Your code
+require APP_PATH . "App.php";
+
+$files = getTransactionFiles(FILES_PATH);
+
+$transactions = [];
+
+foreach($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file));
+}
+
+require VIEWS_PATH . "transactions.php";
